f33ff1c5b0dad537a43189a9246dda2f8059045b
mlslsenarathna <senarat-ps19286@stu.kln.ac.lk>
2025-11-02 10:20:59 +0530

    customer package complete

--- src/main/java/ecom/mlslsenarathna/controller/CustomerController.java
+++ src/main/java/ecom/mlslsenarathna/controller/CustomerController.java
@@ -1,14 +1,41 @@
 package ecom.mlslsenarathna.controller;
 
-import ecom.mlslsenarathna.repository.CustomerRepository;
+import ecom.mlslsenarathna.model.dto.CustomerDTO;
 import ecom.mlslsenarathna.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
     @Autowired
     CustomerService customerService;
+
+    @GetMapping("/getById/{custId}")
+    public CustomerDTO getCustomer(@PathVariable String custId){
+        return customerService.getCustomer(custId);
+
+    }
+    @GetMapping("/getAllCustomers")
+    public List<CustomerDTO> getAllCustomers(){
+        return customerService.getAllCustomers();
+
+    }
+    @PostMapping("/addCustomer")
+    public void addCustomer(@RequestBody CustomerDTO customerDTO){
+        customerService.add(customerDTO);
+    }
+    @PutMapping("/Update{customerDTO}")
+    public void updateByID(@RequestBody CustomerDTO customerDTO){
+        customerService.updateByID(customerDTO);
+    }
+    @DeleteMapping("/deleteById/{customerId}")
+    public void deleteById(@PathVariable String customerId){
+        customerService.deleteById(customerId);
+
+    }
+
+
 }
--- src/main/java/ecom/mlslsenarathna/service/CustomerService.java
+++ src/main/java/ecom/mlslsenarathna/service/CustomerService.java
@@ -1,11 +1,72 @@
 package ecom.mlslsenarathna.service;
 
+import ecom.mlslsenarathna.model.dto.CustomerDTO;
+import ecom.mlslsenarathna.model.entity.CustomerEntity;
 import ecom.mlslsenarathna.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
+
+    public CustomerDTO getCustomer(String custId) {
+        Optional<CustomerEntity> custbyID=customerRepository.findById(custId);
+        CustomerEntity customerEntity=custbyID.orElseThrow();
+        return new CustomerDTO(
+                customerEntity.getCustId(),
+                customerEntity.getCustName(),
+                customerEntity.getCustAddress(),
+                customerEntity.getCustMobbileNumber()
+        );
+
+    }
+
+    public List<CustomerDTO> getAllCustomers() {
+        List<CustomerEntity> customerEntityList=customerRepository.findAll();
+        List<CustomerDTO> customersList= new ArrayList<>();
+        for(CustomerEntity customerEntity:customerEntityList){
+            customersList.add(new CustomerDTO(
+                    customerEntity.getCustId(),
+                    customerEntity.getCustName(),
+                    customerEntity.getCustAddress(),
+                    customerEntity.getCustMobbileNumber()
+
+            ));
+
+
+        }
+        return customersList;
+    }
+
+    public void add(CustomerDTO customerDTO) {
+        customerRepository.save(
+                new CustomerEntity(
+                        customerDTO.getCustId(),
+                        customerDTO.getCustName(),
+                        customerDTO.getCustAddress(),
+                        customerDTO.getCustMobbileNumber()
+                )
+        );
+    }
+
+    public void updateByID(CustomerDTO customerDTO) {
+        customerRepository.save(
+                new CustomerEntity(
+                        customerDTO.getCustId(),
+                        customerDTO.getCustName(),
+                        customerDTO.getCustAddress(),
+                        customerDTO.getCustMobbileNumber()
+                )
+        );
+    }
+
+    public void deleteById(String customerId) {
+        customerRepository.deleteById(customerId);
+    }
 }
